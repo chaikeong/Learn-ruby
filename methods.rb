0fc685f64bc43ce3c4198b555c4576d227ccfbce
chaikeong <tangchaikeong@gmail.com>
2012-02-28 23:52:34 +0800

    methods

---
+++ methods.rb
@@ -0,0 +1,18 @@
+puts self
+puts self.class
+
+msg = 'hello world'
+puts msg.reverse
+puts msg
+puts msg.reverse!
+puts msg
+
+msg = "good morning"
+puts "length=#{msg.length}"
+puts msg.center(50)
+
+#rude boss
+puts "WHAT DO U WANT?!"
+msg = gets.chomp.upcase
+puts "WHADDAYA MEAN \"" + msg + "\"?!? YOU'RE FIRED!!"
+
