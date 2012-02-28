2ff156145ff8710966bc28069f150106fd2f42fc
chaikeong <tangchaikeong@gmail.com>
2012-02-28 22:16:18 +0800

    Numbers

---
+++ numbers.rb
@@ -0,0 +1,10 @@
+puts 1+2
+puts 9/4
+puts 9.0/2
+puts 9/2.0
+
+puts "hours in a year:= #{(365*24)}"
+puts "minutes in a decade:= #{(10*165*24*60)}"
+
+
+puts "age of 1,025 million second:= #{((1025.0*1000000)/(365*24*60*60))} years"
