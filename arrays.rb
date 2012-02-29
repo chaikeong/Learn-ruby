3e0d403a294a374d1039034f774925a7220c7704
chaikeong <tangchaikeong@gmail.com>
2012-02-29 22:50:15 +0800

    arrays

---
+++ arrays.rb
@@ -0,0 +1,17 @@
+countries = ["america", "britain", "canada", "denmark"]
+
+countries.each do |country|
+  puts country.reverse
+end
+
+puts
+puts countries.last
+puts countries.first
+
+puts
+puts countries.pop
+
+puts
+puts countries
+
+puts countries.join(",")
