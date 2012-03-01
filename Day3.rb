a97e95bbab213efaccac13a1d6249a9725e405d4
chaikeong <tangchaikeong@gmail.com>
2012-03-01 22:38:16 +0800

    Day 3

---
+++ Day3.rb
@@ -0,0 +1,20 @@
+my_array = ["one", "two", "three", "four"]
+my_hash = {"adam" => "apple", "ben" => "b...h", "charlie" => "cat"}
+
+my_array.each do |item|
+  puts item
+end
+
+my_hash.each do |key, value|
+  puts "#{key} : #{value}"
+end
+
+puts
+msg = "the quick brown fox jumps over the lazy dog."
+puts msg[2, 8]
+puts msg[2..8]
+puts msg[8, 2]
+puts msg[2...8]
+puts msg[-8...-1]
+puts msg[-8..-1]
+
